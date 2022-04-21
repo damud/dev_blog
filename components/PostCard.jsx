0443318a3373f21b4e5ab9c61865437515b4b5e2
Daniel Mudlaff <dmudlaff89@gmail.com>
2022-04-21 12:48:49 +0200

    Add PostCard, Categories & PostWidget components

---
+++ components/PostCard.jsx
@@ -0,0 +1,10 @@
+function PostCard({ post }) {
+  return (
+    <div>
+      {post.title}
+      {post.excerpt}
+    </div>
+  )
+}
+
+export default PostCard
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,32 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { PostCard, Categories, PostWidget } from '../components'
+
+const posts = [
+  { title: 'React Testing', excerpt: 'Learn React Testing' },
+  { title: 'React with Tailwind', excerpt: 'React with Tailwind' },
+]
 
 const Home: NextPage = () => {
   return (
-    <div className="container mx-auto px-10 mb-8">
+    <div className="container mx-auto mb-8 px-10">
       <Head>
         <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map((post) => (
+            <PostCard post={post} key={post.title} />
+          ))}
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative top-8 lg:sticky">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
