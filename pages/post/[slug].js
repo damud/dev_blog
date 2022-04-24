ed575bf14c40ad756446fc37b99711b5d9289a04
Daniel Mudlaff <dmudlaff89@gmail.com>
2022-04-24 14:44:32 +0200

    Display post in selected category

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 import {
   PostDetail,
@@ -7,10 +8,15 @@ import {
   Author,
   Comments,
   CommentsForm,
+  Loader,
 } from '../../components'
 import { getPosts, getPostDetails } from '../../services'
 
 const PostDetails = ({ post }) => {
+  const router = useRouter()
+  if (router.isFallback) {
+    return <Loader />
+  }
   return (
     <>
       <div className="container mx-auto mb-8 px-10">
@@ -37,7 +43,6 @@ const PostDetails = ({ post }) => {
 }
 export default PostDetails
 
-// Fetch data at build time
 export async function getStaticProps({ params }) {
   const data = await getPostDetails(params.slug)
   return {
@@ -47,8 +52,6 @@ export async function getStaticProps({ params }) {
   }
 }
 
-// Specify dynamic routes to pre-render pages based on data.
-// The HTML is generated at build time and will be reused on each request.
 export async function getStaticPaths() {
   const posts = await getPosts()
   return {
